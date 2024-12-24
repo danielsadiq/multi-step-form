8d068cb2757e6dd8eef8afdfa09908b752f0f1ef
Daniel Sadiq <danielsadiq93@gmail.com>
2024-12-24 21:11:24 +0100

    hook form add

--- src/ui/FormRow.jsx
+++ src/ui/FormRow.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/prop-types */
-function FormRow({name, placeholder}) {
+function FormRow({ name, placeholder, register }) {
   return (
     <div className="grid items-center py-2 space-y-2">
       <label className="font-normal text-marineblue">{name}</label>
-      <input placeholder={placeholder} className="border-[2px] rounded-md px-3 py-2 border-lightGray" />
+      <input
+        placeholder={placeholder}
+        className="border-[2px] rounded-md px-3 py-2 border-lightGray"
+        {...register(name.replace(" ", ""))}
+      />
     </div>
-  )
+  );
 }
 
-export default FormRow
+export default FormRow;
--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -1,16 +1,29 @@
 import FormRow from "./FormRow";
+import { useForm } from "react-hook-form"
 
 function Main() {
+  const {register, handleSubmit} = useForm();
+  function onSubmit(data){
+    console.log(data);
+  }
   return (
-    <div className="text-left p-4"> 
+    <div className="text-left p-4">
       <h2 className="text-xl font-bold text-marineblue">Personal info</h2>
       <p className="text-coolGray">
         Please provide your name, email address, and phone number.
       </p>
-      <FormRow name={"Name"} placeholder={"e.g. Stephen King"} />
-      <FormRow name={"Email Address"} placeholder={"e.g. stephenking@lorem.com"} />
-      <FormRow name={"Phone Number"} placeholder={"e.g. +1 234 567 890"} />
-        Next Step
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormRow name={"Name"} placeholder={"e.g. Stephen King"} register={register} />
+        <FormRow
+          name={"Email Address"}
+          placeholder={"e.g. stephenking@lorem.com"}
+          register={register}
+        />
+        <FormRow name={"Phone Number"} placeholder={"e.g. +1 234 567 890"} register={register} />
+        <button className="px-4 py-2 bg-marineblue text-white float-right mt-4">
+          Next Step
+        </button>
+      </form>
     </div>
   );
 }
