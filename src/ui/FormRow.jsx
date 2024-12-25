bf41fe8a70373c63c8414184493939edb2c0b80a
Daniel Sadiq <danielsadiq93@gmail.com>
2024-12-25 11:38:17 +0100

    errors add

--- src/ui/FormRow.jsx
+++ src/ui/FormRow.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
-function FormRow({ name, placeholder, register }) {
+function FormRow({ name, placeholder, register, error=false }) {
   const nameForField = name.replace(" ", "");
   return (
     <div className="grid items-center py-2 space-y-2">
-      <label className="font-normal text-marineblue">{name}</label>
+      <div className="flex justify-between">
+        <label className="font-normal text-marineblue">{name}</label>
+        <p className="text-">{error!=false ? error : ""}</p>
+      </div>
       <input
         placeholder={placeholder}
+        id={nameForField}
         className="border-[2px] rounded-md px-3 py-2 border-lightGray"
-        {...register(nameForField, {required:"This field is required"})}
+        {...register(nameForField, { required: "This field is required" })}
       />
     </div>
   );
--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -1,12 +1,17 @@
 import FormRow from "./FormRow";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 
 function Main() {
-  const {register, handleSubmit} = useForm();
-  function onSubmit(data){
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  function onSubmit(data) {
     console.log(data);
   }
-  function onError(errors){
+  function onError(error) {
+    console.log(error);
     console.log(errors);
   }
   return (
@@ -16,13 +21,24 @@ function Main() {
         Please provide your name, email address, and phone number.
       </p>
       <form onSubmit={handleSubmit(onSubmit, onError)}>
-        <FormRow name={"Name"} placeholder={"e.g. Stephen King"} register={register} />
+        <FormRow
+          name={"Name"}
+          placeholder={"e.g. Stephen King"}
+          register={register}
+          error={errors?.Name?.message}
+        />
         <FormRow
           name={"Email Address"}
           placeholder={"e.g. stephenking@lorem.com"}
           register={register}
+          error={errors?.EmailAddress?.message}
         />
-        <FormRow name={"Phone Number"} placeholder={"e.g. +1 234 567 890"} register={register} />
+        <FormRow
+          name={"Phone Number"}
+          placeholder={"e.g. +1 234 567 890"}
+          register={register}
+          error={errors?.PhoneNumber?.message}
+        />
         <button className="px-4 py-2 bg-marineblue text-white float-right mt-4">
           Next Step
         </button>
