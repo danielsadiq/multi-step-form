4cba71cfc8fb548f842920f751637f8792a8705e
Daniel Sadiq <danielsadiq93@gmail.com>
2025-01-01 21:16:19 +0100

    new year

--- src/pages/AddOns.jsx
+++ src/pages/AddOns.jsx
@@ -5,8 +5,12 @@ function AddOns() {
       <p className="text-coolGray">
         Add-ons help enhance your gaming experience
       </p>
+      <div>Online service
+        Access to multiplayer games +$1/mo Larger storage Extra 1TB of cloud
+        save +$2/mo Customizable Profile Custom theme on your profile +$2/mo
+      </div>
     </div>
-  )
+  );
 }
 
-export default AddOns
+export default AddOns;
